bbf485719d89a64b0c411891e3ead3faf6b22a26
이서연 <redchk35@naver.com>
2025-04-06 19:22:20 +0900

    week4 mission2 commit

---
+++ week4/mission2/mission2/src/App.tsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomeLayout from "./layouts/HomeLayout";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
+// 1. 홈페이지
+// 2. 로그인 페이지
+// 3. 회원가입 페이지
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <div>Error</div>,
+    children:[
+      { index: true, element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "signup", element: <SignupPage /> },  
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
