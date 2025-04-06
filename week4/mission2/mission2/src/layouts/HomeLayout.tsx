0303d47e3f10b433b43b5daf5411c01a69236d47
이서연 <redchk35@naver.com>
2025-04-06 20:42:22 +0900

    week4 mission2 commit

--- week4/mission2/mission2/src/layouts/HomeLayout.tsx
+++ week4/mission2/mission2/src/layouts/HomeLayout.tsx
@@ -3,11 +3,16 @@ import { Outlet } from "react-router-dom";
 const HomeLayout = () => {
     return (
         <div className="h-dvh flex flex-col">
-            <nav>네비게이션 바 입니다.</nav>
+            <nav className="h-20 flex items-center justify-between px-6 bg-gray-900 text-white">
+                <div className="text-pink-500 font-bold text-2xl">돌려돌려LP판</div>
+                <div className="flex gap-2">
+                    <button className="bg-black text-white border border-white px-4 py-2 rounded-md hover:bg-white hover:text-black transition">로그인</button>
+                    <button className="bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-600 transition">회원가입</button>
+                </div>
+            </nav>
             <main className="flex-1">
                 <Outlet />
             </main>
-            <footer>푸터</footer>
         </div>
     );
 };
--- week4/mission2/mission2/src/utils/validate.ts
+++ week4/mission2/mission2/src/utils/validate.ts
@@ -12,12 +12,12 @@ function validateUser(values: UserSigninInformation) {
     if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(
         values.email,
     )) {
-        errors.email = "올바른 이메일 형식이 아닙니다.";
+        errors.email = "올바른 이메일 형식을 입력해주세요.";
     }
 
     // 비밀번호 8자 20자 사이
     if (!(values.password.length >= 8 && values.password.length <= 20)) {
-        errors.password = "비밀번호는 8~20자 사이로 입력해주세요.";
+        errors.password = "비밀번호는 8자 이상이어야 합니다.";
     }
 
     return errors;
