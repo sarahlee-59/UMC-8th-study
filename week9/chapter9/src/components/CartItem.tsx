14e63feef08a4611daa48770bb4894915a6c921d
이서연 <redchk35@naver.com>
2025-05-25 15:28:00 +0900

    week9 mission1 commit

--- week9/chapter9/src/components/CartItem.tsx
+++ week9/chapter9/src/components/CartItem.tsx
@@ -1,19 +1,34 @@
-import { Lp } from '../types/cart';
+import type { Lp } from '../types/cart';
 
 interface CartItemProps {
     lp: Lp;
 }
 
-const CartItem = ({lp} : CartItemProps) : Element => {
+const CartItem = ({ lp } : CartItemProps) : Element => {
     return (
-        <div className='flex'>
+        <div className='flex items-center p-4 border-b border-gray-200'>
             <img
             src={lp.img}
             alt={`${lp.title}의 LP 이미지`}
             className='w-20 h-20 object-cover rounded mr-4'
             />
-            <div>
-                <h3 className='text-xl'>{lp.title}</h3>
+            <div className='flex-1'>
+                <h3 className='text-xl font-semibold'>{lp.title}</h3>
+                <p className='text-sm text-gray-600'>{lp.singer}</p>
+                <p className='text-sm font-bold text-gray-600'>{lp.price}원</p>
+            </div>
+            <div className='flex items-center'>
+                <button className='px-3 py-1 bg-gray-300 text-gray-800 rounded-l
+                hover:bg-gray-400 cursor-pointer'>
+                    -
+                </button>
+                <span className='px-4 py-[3px] border-y border-gray-300'>
+                    {lp.amount}
+                </span>
+                <button className='px-3 py-1 bg-gray-300 text-gray-800 rounded-r
+                hover:bg-gray-400 cursor-pointer'>
+                    +
+                </button>
             </div>
         </div>
     );
